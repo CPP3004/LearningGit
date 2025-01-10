827008ebe24ab857e35bac2c4c1a4ae3b690876d
Cpp3004 <cpp3004@gmail.com>
2025-01-10 16:18:22 -0500

    More Python Changes

--- Python_Tommorow.py
+++ Python_Tommorow.py
@@ -3,3 +3,4 @@ from datetime import date
 today=date.today()
 days=["Sun","Mon","Tue","Wed","Thu","Fri","Sat"]
 print("Tomorrow will be " +days[today.weekday()+1 %7])
+print("Heeeeeeeeeeeeee Hawwwwwwwwwwwwwwww")
